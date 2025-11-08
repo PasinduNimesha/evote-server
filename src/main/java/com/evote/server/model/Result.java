b56d3049dcf0998f055faa4ff21090375acd6c74
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2025-11-08 09:46:01 +0530

    Create Result.java

---
+++ src/main/java/com/evote/server/model/Result.java
@@ -0,0 +1,28 @@
+package com.evote.server.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "result")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+
+    @Id
+    @Column(name = "candidate_id")
+    private Long candidateId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "total_vote", nullable = false)
+    @Builder.Default
+    private Integer totalVote = 0;
+
+    @OneToOne
+    @JoinColumn(name = "candidate_id", insertable = false, updatable = false)
+    private Candidate candidate;
+}
