d5a735beebd116267a42c165583687d14bc4d64b
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2025-11-11 10:13:47 +0530

    Create RemoveCandidateFromElectionRequest.java

---
+++ src/main/java/com/evote/server/dto/RemoveCandidateFromElectionRequest.java
@@ -0,0 +1,20 @@
+package com.evote.server.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RemoveCandidateFromElectionRequest {
+
+    @NotNull(message = "Election ID is required")
+    private Long electionId;
+
+    @NotNull(message = "Candidate ID is required")
+    private Long candidateId;
+}
