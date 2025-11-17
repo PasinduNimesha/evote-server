770a826efaa107867b4c72e451eb62422e9765b1
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2025-11-17 23:48:58 +0530

    Create ElectionRepository.java

---
+++ src/main/java/com/evote/server/repository/ElectionRepository.java
@@ -0,0 +1,9 @@
+package com.evote.server.repository;
+
+import com.evote.server.model.Election;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ElectionRepository extends JpaRepository<Election, Long> {
+}
